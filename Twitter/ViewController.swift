600698e3dc146de4177b5540b585d132f8e5d66b
Yeu-Shuan Tang <ytang@zendesk.com>
2015-02-22 16:39:05 -0800

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Yeu-Shuan Tang on 2/22/15.
+//  Copyright (c) 2015 Yeu-Shuan Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
